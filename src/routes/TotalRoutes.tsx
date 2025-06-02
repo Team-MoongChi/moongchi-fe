9a6213e4b7732bbd7f695bcb7b3fcc972e2a7288
sonnnnhe <147504410+sonnnnhe@users.noreply.github.com>
2025-06-02 22:49:34 +0900

    feat: 전체 라우팅 관리 파일 추가

---
+++ src/routes/TotalRoutes.tsx
@@ -0,0 +1,11 @@
+import { BrowserRouter } from "react-router-dom";
+
+import GongguMainRoutes from "./gongguPage/GongguMainRoutes";
+
+export default function TotalRoutes() {
+    return (
+        <BrowserRouter>
+            <GongguMainRoutes />
+        </BrowserRouter>
+    );
+}
