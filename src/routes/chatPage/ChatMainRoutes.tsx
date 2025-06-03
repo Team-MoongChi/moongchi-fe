8f6c84a5f1634955d3b0c779b0c9826f056c5de7
sonnnnhe <147504410+sonnnnhe@users.noreply.github.com>
2025-06-03 23:05:16 +0900

    feat: 결제 및 리뷰 페이지 이동 라우팅 추가

--- src/routes/chatPage/ChatMainRoutes.tsx
+++ src/routes/chatPage/ChatMainRoutes.tsx
@@ -1,11 +1,17 @@
 import { Routes, Route } from "react-router-dom";
 
-import ChatListPage from "../../pages/chatPages/chatListPage";
+import ChatListPage from "../../pages/chatPages/ChatListPage";
+import ChatPayPage from "../../pages/chatPages/ChatPayPage";
+import AllReviewPage from "../../pages/chatPages/AllReviewPage";
+import OneReviewPage from "../../pages/chatPages/OneReviewPage";
 
 export default function ChatMainRoutes() {
     return (
         <Routes>
             <Route path="/chat/list" element={<ChatListPage />}></Route>
+            <Route path="/chat/pay" element={<ChatPayPage />}></Route>
+            <Route path="/chat/review" element={<AllReviewPage />}></Route>
+            <Route path="/chat/review/write" element={<OneReviewPage />}></Route>
         </Routes>
     );
 }
