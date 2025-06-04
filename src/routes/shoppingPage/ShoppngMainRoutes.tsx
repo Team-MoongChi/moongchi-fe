6ea2d4cc69e550441859ef1ac1afc61902bc03e8
지원 <wi1767@naver.com>
2025-06-04 13:21:10 +0900

    feat: 쇼핑 페이지 루트 추가

---
+++ src/routes/shoppingPage/ShoppngMainRoutes.tsx
@@ -0,0 +1,15 @@
+import { Routes, Route } from "react-router-dom";
+
+import ShopItemPage from "../../pages/shoppingPages/ShopItemPage";
+import ShopMainPage from "../../pages/shoppingPages/ShopMainPage";
+import ShopResultPage from "../../pages/shoppingPages/ShopResultPage";
+
+export default function ShoppingMainRoutes() {
+  return (
+    <Routes>
+      <Route path="/shopping" element={<ShopMainPage />}></Route>
+      <Route path="/shopping/result" element={<ShopResultPage />}></Route>
+      <Route path="/shopping/item" element={<ShopItemPage />}></Route>
+    </Routes>
+  );
+}
