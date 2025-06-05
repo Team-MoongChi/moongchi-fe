2b1fd82cc2c417da9d13ab45208cdbdff34937ee
sonnnnhe <fladi39@naver.com>
2025-06-05 10:19:24 +0900

    types: 공구 목록 및 상세 조회 데이터 타입 정의

---
+++ src/types/gongguPage/gongguItem.ts
@@ -0,0 +1,40 @@
+// 타입 정의
+type BoardStatus = "OPEN" | "CLOSING_SOON" | "CLOSED" | "COMPLETED";
+interface Category {
+  id: number;
+  name: string;
+  level: "LARGE" | "MEDIUM" | "SMALL";
+  subCategories?: Category[];
+}
+interface ShopProduct {
+  id: number;
+  imgUrl: string;
+  productUrl: string;
+}
+interface Product {
+  id: number;
+  name: string;
+  price: number;
+  quantity: string;
+  images: string[];
+  product?: ShopProduct;
+  category: Category;
+}
+interface Participant {
+  id: number;
+  userId: number;
+  name: string;
+  role: "LEADER" | "MEMEBER";
+}
+export interface GongguItem {
+  id: number;
+  title: string;
+  content: string;
+  location: string;
+  boardStatus: BoardStatus;
+  deadline: string;
+  totalUsers: number;
+  currentUsers?: number;
+  groupProduct: Product;
+  participants: Participant[];
+}
