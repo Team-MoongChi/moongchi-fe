bd628b6f41feb92ef89b33aab490f5fa24e13b34
sonnnnhe <fladi39@naver.com>
2025-06-11 14:06:47 +0900

    feat: 채팅 관련 라우팅 설정

--- src/routes/chatPage/ChatMainRoutes.tsx
+++ src/routes/chatPage/ChatMainRoutes.tsx
@@ -4,11 +4,13 @@ import ChatListPage from "../../pages/chatPages/ChatListPage";
 import ChatPayPage from "../../pages/chatPages/ChatPayPage";
 import AllReviewPage from "../../pages/chatPages/AllReviewPage";
 import OneReviewPage from "../../pages/chatPages/OneReviewPage";
+import ChatPage from "../../pages/chatPages/ChatPage";
 
 export default function ChatMainRoutes() {
   return (
     <>
       <Route path="/chat/list" element={<ChatListPage />}></Route>
+      <Route path="/chat/list/:chatRoomId" element={<ChatPage />}></Route>
       <Route path="/chat/pay/:chatRoomId" element={<ChatPayPage />}></Route>
       <Route path="/chat/review" element={<AllReviewPage />}></Route>
       <Route path="/chat/review/write" element={<OneReviewPage />}></Route>
