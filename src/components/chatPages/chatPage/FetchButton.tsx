b5812ac1efcec0636e54fb707e364ff5f5b6d663
sonnnnhe <fladi39@naver.com>
2025-06-11 14:12:36 +0900

    design: 채팅 프로세스 진행 버튼

---
+++ src/components/chatPages/chatPage/FetchButton.tsx
@@ -0,0 +1,36 @@
+import styled from "styled-components";
+import { Text } from "../../common/styled-component/Text";
+import { Link } from "react-router-dom";
+
+const Wrap = styled.div`
+  display: inline-block;
+  width: 30%;
+  align-self: center;
+  background-color: #5849d0;
+  border-radius: 20px;
+  padding: 20px 0;
+  text-align: center;
+`;
+
+interface ButtonProps {
+  content: string;
+  link?: string;
+}
+
+export default function FetchButton(props: ButtonProps) {
+  return (
+    <Wrap>
+      {props.link ? (
+        <Link to={props.link}>
+          <Text fontSize="15px" fontFamily="DunggeunmisoBold" color="white">
+            {props.content}
+          </Text>
+        </Link>
+      ) : (
+        <Text fontSize="15px" fontFamily="DunggeunmisoBold" color="white">
+          {props.content}
+        </Text>
+      )}
+    </Wrap>
+  );
+}
