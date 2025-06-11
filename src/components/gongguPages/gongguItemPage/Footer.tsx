24c65828f05ec9735607d0614784a13d89306ba4
sonnnnhe <fladi39@naver.com>
2025-06-11 23:48:51 +0900

    feat: 공구 참여 API 연결

--- src/components/gongguPages/gongguItemPage/Footer.tsx
+++ src/components/gongguPages/gongguItemPage/Footer.tsx
@@ -1,5 +1,7 @@
 import styled from "styled-components";
 import useDeviceSize from "../../../useDeviceSize";
+import { useNavigate, useParams } from "react-router-dom";
+import { fetchWithAuth } from "../../../utils/FetchWithAuth";
 
 const Wrap = styled.div<{ $issmall: boolean }>`
   width: ${(props) => (props.$issmall ? "100%" : "50%")};
@@ -20,12 +22,50 @@ const Button = styled.div`
   border-radius: 15px;
 `;
 
-export default function Footer() {
+interface FooterProp {
+  editable: boolean | undefined;
+  chatRoomId: number | undefined;
+}
+
+export default function Footer(props: FooterProp) {
   const { small } = useDeviceSize();
+  const { gongguId } = useParams();
+  const navigate = useNavigate();
+
+  const GotoChat = async () => {
+    const token = localStorage.getItem("access_token");
+    console.log(token);
+
+    try {
+      const response = await fetchWithAuth(
+        `http://localhost:8080/api/group-boards/${gongguId}/join`,
+        {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+        }
+      );
+      if (response.ok) {
+        alert("공구방 참여 성공");
+        navigate(`/chat/list/${props.chatRoomId}`);
+      }
+    } catch (error) {
+      console.error("post failed: ", error);
+      alert("공구방 참여 실패");
+      throw error;
+    }
+  };
 
   return (
     <Wrap $issmall={small}>
-      <Button>공구 함께하기</Button>
+      {props.editable ? (
+        <Button onClick={() => navigate(`/chat/list/${props.chatRoomId}`)}>
+          채팅방 바로 가기
+        </Button>
+      ) : (
+        <Button onClick={GotoChat}>공구 참여하기</Button>
+      )}
     </Wrap>
   );
 }
