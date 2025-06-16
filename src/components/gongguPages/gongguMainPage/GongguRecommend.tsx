480958cdb2ef8779b68458bb1ba4c14ce0be4f8e
sonnnnhe <fladi39@naver.com>
2025-06-16 14:10:07 +0900

    test: 상품 / 자체 공구 구분을 위한 테스트 코드

--- src/components/gongguPages/gongguMainPage/GongguRecommend.tsx
+++ src/components/gongguPages/gongguMainPage/GongguRecommend.tsx
@@ -1,10 +1,10 @@
 import styled from "styled-components";
 import { Text } from "../../common/styled-component/Text";
+import { useNavigate } from "react-router-dom";
 
 const Wrap = styled.div`
   display: flex;
   flex-direction: column;
-  padding: 0 5%;
   gap: 10px;
 `;
 const RecList = styled.div`
@@ -15,8 +15,21 @@ const RecList = styled.div`
 `;
 
 export default function GongguRecommend() {
+  const navigate = useNavigate();
+  const writeGonggu2 = () => {
+    navigate("/gonggu/write", {
+      state: {
+        message: "shop",
+        imgUrl:
+          "https://sitem.ssgcdn.com/80/96/87/item/1000039879680_i1_332.jpg",
+        name: "딸기 2박스",
+        categoryId: 2,
+      },
+    });
+  };
+
   return (
-    <Wrap>
+    <Wrap onClick={writeGonggu2}>
       <Text fontSize="20px" fontFamily="DunggeunmisoBold" color="#5849d0">
         뭉치's PICK
       </Text>
