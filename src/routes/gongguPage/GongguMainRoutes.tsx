8ae88a06611ea4bfb5179d6e09b91eb009902c3e
sonnnnhe <fladi39@naver.com>
2025-06-16 14:01:46 +0900

    feat: 공구 작성 > 위치 추가 페이지 라우팅

--- src/routes/gongguPage/GongguMainRoutes.tsx
+++ src/routes/gongguPage/GongguMainRoutes.tsx
@@ -2,6 +2,7 @@ import { Route } from "react-router-dom";
 
 import GongguMainPage from "../../pages/gongguPages/GongguMainPage";
 import GongguWritePage from "../../pages/gongguPages/GongguWritePage";
+import GongguMapPage from "../../pages/gongguPages/GongguMapPage";
 import GongguItemPage from "../../pages/gongguPages/GongguItemPage";
 
 export default function GongguMainRoutes() {
@@ -9,6 +10,7 @@ export default function GongguMainRoutes() {
     <>
       <Route path="/" element={<GongguMainPage />}></Route>
       <Route path="/gonggu/write" element={<GongguWritePage />}></Route>
+      <Route path="/gonggu/write/map" element={<GongguMapPage />}></Route>
       <Route path="/gonggu/list/:gongguId" element={<GongguItemPage />}></Route>
     </>
   );
