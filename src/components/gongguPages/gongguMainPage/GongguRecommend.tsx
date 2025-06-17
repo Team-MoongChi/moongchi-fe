a4202e7b3e1d4ea4de911ff3edcc372c50d0455a
sonnnnhe <fladi39@naver.com>
2025-06-17 23:34:37 +0900

    refactor: 불필요한 코드 삭제

--- src/components/gongguPages/gongguMainPage/GongguRecommend.tsx
+++ src/components/gongguPages/gongguMainPage/GongguRecommend.tsx
@@ -1,6 +1,5 @@
 import styled from "styled-components";
 import { Text } from "../../common/styled-component/Text";
-import { useNavigate } from "react-router-dom";
 
 const Wrap = styled.div`
   display: flex;
@@ -15,21 +14,8 @@ const RecList = styled.div`
 `;
 
 export default function GongguRecommend() {
-  const navigate = useNavigate();
-  const writeGonggu2 = () => {
-    navigate("/gonggu/write", {
-      state: {
-        message: "shop",
-        imgUrl:
-          "https://sitem.ssgcdn.com/80/96/87/item/1000039879680_i1_332.jpg",
-        name: "딸기 2박스",
-        categoryId: 2,
-      },
-    });
-  };
-
   return (
-    <Wrap onClick={writeGonggu2}>
+    <Wrap>
       <Text fontSize="20px" fontFamily="DunggeunmisoBold" color="#5849d0">
         뭉치's PICK
       </Text>
