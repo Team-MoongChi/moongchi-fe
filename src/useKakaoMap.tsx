e8f2bd8325182af989a57f1cf3dc0ee5b3173fe4
sonnnnhe <fladi39@naver.com>
2025-06-17 23:27:28 +0900

    refactor: 카카오맵 API 변수명 변경

--- src/useKakaoMap.tsx
+++ src/useKakaoMap.tsx
@@ -2,7 +2,7 @@ import { useKakaoLoader } from "react-kakao-maps-sdk";
 
 export default function useKakaoMap() {
   return useKakaoLoader({
-    appkey: import.meta.env.VITE_KAKAOMAP_KEY,
+    appkey: import.meta.env.VITE_KAKAOMAP_API_KEY,
     libraries: ["clusterer", "drawing", "services"],
   });
 }
