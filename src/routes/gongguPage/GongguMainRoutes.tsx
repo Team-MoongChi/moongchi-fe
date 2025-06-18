f7984d1208fb69fd465c684f7dc99957830a60bd
sonnnnhe <fladi39@naver.com>
2025-06-18 19:46:02 +0900

    feat: 공구 지도 라우팅 추가

--- src/routes/gongguPage/GongguMainRoutes.tsx
+++ src/routes/gongguPage/GongguMainRoutes.tsx
@@ -4,6 +4,7 @@ import GongguMainPage from "../../pages/gongguPages/GongguMainPage";
 import GongguSearchPage from "../../pages/gongguPages/GongguSearchPage";
 import GongguItemPage from "../../pages/gongguPages/GongguItemPage";
 import GongguWritePage from "../../pages/gongguPages/GongguWritePage";
+import GongguMapPage from "../../pages/gongguPages/GongguMapPage";
 
 export default function GongguMainRoutes() {
   return (
@@ -16,6 +17,7 @@ export default function GongguMainRoutes() {
         path="/gonggu/edit/:gongguId"
         element={<GongguWritePage />}
       ></Route>
+      <Route path="/gonggu/map" element={<GongguMapPage />}></Route>
     </>
   );
 }
