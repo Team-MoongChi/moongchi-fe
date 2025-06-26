53dc601def5c23457844ac8c521f75b4872ce131
sonnnnhe <fladi39@naver.com>
2025-06-26 13:47:07 +0900

    fix: d-day 날짜 계산 함수 수정

--- src/hooks/useCalDay.tsx
+++ src/hooks/useCalDay.tsx
@@ -4,12 +4,26 @@ export default function useCalDay(timestamp: string): string {
   const [timeState, setTimeState] = useState<string>("");
 
   const updateTimeStamp = () => {
-    const timeLeft = Math.floor(
-      (new Date(timestamp).getTime() - new Date().getTime()) / 1000
-    );
+    const targetDate = new Date(timestamp);
+    const now = new Date();
 
-    const days = Math.floor(timeLeft / (60 * 60 * 24));
-    setTimeState(`D-${days}`);
+    // 오늘 날짜 가져오기 (시간 0시 0분 0초)
+    const today = Date.UTC(now.getFullYear(), now.getMonth(), now.getDate());
+
+    // 비교할 날짜 가져오기 (시간 0시 0분 0초)
+    const target = Date.UTC(
+      targetDate.getFullYear(),
+      targetDate.getMonth(),
+      targetDate.getDate()
+    );
+    console.log(today, target);
+
+    const days = Math.floor((target - today) / (1000 * 60 * 60 * 24));
+    if (days === 0) {
+      setTimeState(`D-DAY`);
+    } else {
+      setTimeState(`D-${days}`);
+    }
   };
 
   useEffect(() => {
