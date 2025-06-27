dd5cb5d7f54b8db53f0c26dbef19ca1116158279
sonnnnhe <fladi39@naver.com>
2025-06-27 19:02:26 +0900

    chore: 변수명 변경

--- src/pages/chatPages/ChatPayResult.tsx
+++ src/pages/chatPages/ChatPayResult.tsx
@@ -9,43 +9,47 @@ export default function ChatPayResult() {
   const location = useLocation();
   const query = new URLSearchParams(location.search);
 
-  const impUid = query.get("imp_uid");
+  const imp_uid = query.get("imp_uid");
   const [loading, setLoading] = useState(true);
   const [message, setMessage] = useState("");
 
-  const verifyPayment = async () => {
-    try {
-      const response = await fetchWithAuth(
-        `/api/chat/rooms/${chatRoomId}/pay`,
-        {
-          method: "POST",
-          headers: { "Content-Type": "application/json" },
-          body: JSON.stringify({ impUid: impUid }),
-        }
-      );
-
-      if (response.ok) {
-        setMessage("결제 성공!");
-        alert("결제성공");
-        navigate(`/chat/${chatRoomId}/pay`);
-      } else {
-        setMessage("결제 검증 실패");
-      }
-    } catch (e) {
-      setMessage("서버 오류로 결제를 검증하지 못했습니다.");
-    } finally {
-      setLoading(false);
-    }
-  };
-
   useEffect(() => {
-    if (!impUid) {
+    if (!imp_uid) {
       setMessage("결제 정보가 없습니다.");
       setLoading(false);
       return;
     }
+    const verifyPayment = async () => {
+      try {
+        const response = await fetchWithAuth(
+          `/api/chat/rooms/${chatRoomId}/pay`,
+          {
+            method: "POST",
+            headers: { "Content-Type": "application/json" },
+            body: JSON.stringify({ impUid: imp_uid }),
+          }
+        );
+
+        if (response.ok) {
+          setMessage("결제 성공!");
+          alert("결제성공");
+          navigate(`/chat/${chatRoomId}/pay`);
+        } else {
+          setMessage("결제 검증 실패");
+        }
+      } catch (e) {
+        setMessage("서버 오류로 결제를 검증하지 못했습니다.");
+      } finally {
+        setLoading(false);
+      }
+    };
+
     verifyPayment();
-  }, []);
+  }, [imp_uid, chatRoomId]);
+
+  useEffect(() => {
+    console.log("impUid", imp_uid);
+  }, [imp_uid]);
 
   if (loading) return <div>결제 검증 중...</div>;
   return <div>{message}</div>;
